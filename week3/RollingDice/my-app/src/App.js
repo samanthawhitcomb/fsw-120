4df54f4f663f6a87629f4b82db0f3a8d736013e3
Sami <Samantha.whitcomb@bryanuniversity.edu>
2020-06-25 22:14:44 -0700

    DICE

---
+++ week3/RollingDice/my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import Dicebox from "./Dicebox"
+
+function App() {
+  return (
+    <div className="App">
+      <Dicebox />
+    </div>
+  );
+}
+
+export default App;
