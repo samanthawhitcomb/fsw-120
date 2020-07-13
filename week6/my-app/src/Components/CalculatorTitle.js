aad391a698024964c03a72f9c70d9a560d0e1e94
Sami <Samantha.whitcomb@bryanuniversity.edu>
2020-07-13 12:42:24 -0700

    calc

---
+++ week6/my-app/src/Components/CalculatorTitle.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function CalculatorTitle(props) {
+    return (
+        <div className="calculatorTitle">
+            {props.value}
+        </div>
+    )
+}
+
+export default CalculatorTitle;
--- week6/my-app/src/index.js
+++ week6/my-app/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './Components/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Calculator />,
   document.getElementById('root')
 );
 
